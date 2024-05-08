e347a7cf54c4c5e5c2c65221ff8103bcc7becd36
Patrick Carvalho <patrickcarvalho448@gmail.com>
2024-05-07 21:07:34 -0300

    experiment and analysis

---
+++ experiments/exp6_2024-05-05/run.sh
@@ -0,0 +1,23 @@
+python ../../main.py -cf config1.json -oh
+python ../../main.py -cf config1.json
+python ../../main.py -cf config1.json
+python ../../main.py -cf config1.json
+python ../../main.py -cf config1.json
+
+python ../../main.py -cf config2.json
+python ../../main.py -cf config2.json
+python ../../main.py -cf config2.json
+python ../../main.py -cf config2.json
+python ../../main.py -cf config2.json
+
+python ../../main.py -cf config3.json
+python ../../main.py -cf config3.json
+python ../../main.py -cf config3.json
+python ../../main.py -cf config3.json
+python ../../main.py -cf config3.json
+
+python ../../main.py -cf config4.json
+python ../../main.py -cf config4.json
+python ../../main.py -cf config4.json
+python ../../main.py -cf config4.json
+python ../../main.py -cf config4.json
